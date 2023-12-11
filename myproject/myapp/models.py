e0a366d420318b2f72e6b72f59719410ea7bfd6e
Petr Mičola <77784828+petr-micola@users.noreply.github.com>
2023-12-11 18:29:50 +0100

    Social login, z username přechod na email.

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True, blank=True, null=True)
+    email = models.EmailField(unique=True, blank=False, null=False, default=None)
     date_of_birth = models.DateField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
 
