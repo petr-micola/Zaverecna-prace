161559e900c64c49fcb41e2b1bedf89a06eaedf4
Petr Mičola <77784828+petr-micola@users.noreply.github.com>
2023-12-26 15:42:44 +0100

    Úprava vzhledu profilové stránky.

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, blank=False, null=False, default=None)
     username = models.CharField(max_length=30, unique=True, blank=False, null=False)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', default='img/Sample_User_Icon.png', blank=True, null=True)
